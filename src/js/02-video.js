b0405a2b07428a14c432d8c86b443328a60ed846
Yevhen Prokhorenko <prokhorenko.yevhen@gmail.com>
2022-07-25 18:41:04 +0300

    fix error git

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,10 +4,12 @@ const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 const LOCALSTORAGE_KEY = "videoplayer-current-time";
 const getTime = localStorage.getItem(LOCALSTORAGE_KEY);
-
+if (getTime !== null) {
+    player.setCurrentTime(getTime);
+}
 
 player.on('timeupdate', throttle(onplay, 1000));
-player.setCurrentTime(getTime);
+
 
 
 function onplay(data) {
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,28 +1,31 @@
 import throttle from 'lodash.throttle';
 const form = document.querySelector('.feedback-form');
 const LOCALSTORAGE_KEY = "feedback-form-state";
-const dataObject = {}
+const dataObject = {};
+
 form.addEventListener("input", throttle(saveEnteredData, 500));
 form.addEventListener("submit", submitData);
 
 updateForm();
 
 function saveEnteredData(e) {
-    const dataObject = { email: e.currentTarget.email.value, message: e.currentTarget.message.value };
-    dataObject = localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(dataObject));
+    dataObject.email = e.target.email.value;
+    dataObject.message = e.target.message.value;
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(dataObject));
 }
 
 function submitData(e) {
     e.preventDefault();
     
-    const dataObject = { email: e.currentTarget.email.value, message: e.currentTarget.message.value };
+    dataObject.email = e.target.email.value;
+    dataObject.message = e.target.message.value;
     console.log(dataObject);
     
     localStorage.removeItem(LOCALSTORAGE_KEY);
     form.reset();
 }
 function updateForm() {      
-    const dataObject = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
-    form.elements.email.value = dataObject.email;
-    form.elements.message.value = dataObject.message;
+    let a = JSON.parse(localStorage.getItem("LOCALSTORAGE_KEY"));
+    form.elements.email.value = a.email;
+    form.elements.message.value = a.message;
 }
