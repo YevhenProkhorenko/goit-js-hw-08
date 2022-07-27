1e1042ecd3ac21ceef40ed417a322560333dab7b
Yevhen Prokhorenko <prokhorenko.yevhen@gmail.com>
2022-07-27 20:22:13 +0300

    fix third task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,45 +1,47 @@
 import throttle from 'lodash.throttle';
 const form = document.querySelector('.feedback-form');
-const inputEl = document.querySelector('input');
-const textEl = document.querySelector('textarea');
+
 const LOCALSTORAGE_KEY = "feedback-form-state";
-const dataObject = {};
 
-form.addEventListener("input", throttle(saveEnteredData, 500));
-updateForm(parseData());
-form.addEventListener("submit", submitData);
+checkForm();
 
+form.addEventListener('input', throttle(saveEnteredData, 500));
 
+function saveEnteredData(event) {
+  event.preventDefault();
 
-function saveEnteredData(e) {
-    if (localStorage) {
-        dataObject[e.target.name] = e.target.value;   
-        localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(dataObject));
-    }    
-    
+  let dataInput = localStorage.getItem(LOCALSTORAGE_KEY)
+   dataInput = dataInput ? JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) : {};
+
+  dataInput[event.target.name] = event.target.value;
+
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(dataInput));
 }
 
-function parseData() {   
-    const savedDataLocStor = localStorage.getItem(LOCALSTORAGE_KEY) || '';
-    if (localStorage) {
-        return JSON.parse(savedDataLocStor);
-    }
-}
+form.addEventListener('submit', submitData);
 
-function updateForm(data) {
-    if (data) {
-        inputEl.value = data.email;
-        textEl.value = data.message;
-    }    
-}
+function submitData(event) {
+  event.preventDefault();
 
-function submitData(e) {
-    e.preventDefault();
-    
-    dataObject.email = e.target.email.value;
-    dataObject.message = e.target.message.value;
-    console.log(dataObject);
-    
+  const email = event.target.elements.email.value;
+  const message = event.target.elements.message.value;
+
+  if (email === '' || message === '') {
+    console.log("Please entered data in all fields");
+  } else {
     localStorage.removeItem(LOCALSTORAGE_KEY);
-    form.reset();
+    console.log({ email, message });
+  }
+
+  event.currentTarget.reset();
+}
+
+function checkForm() {
+  let dataInput = localStorage.getItem(LOCALSTORAGE_KEY);
+  if (dataInput) {
+    dataInput = JSON.parse(dataInput);
+    Object.entries(dataInput).forEach(([name, value]) => {
+      form.elements[name].value = value;
+    });
+  }
 }
